cc8142c095231522244b5b54dd57d90af7a9910e
Aayush  Yadav <aayushyadav388@gmail.com>
2026-08-17 14:22:30 +1000

    Rename Main class to MainFile and update main method

--- MainFile.java
+++ MainFile.java
@@ -1,13 +1,22 @@
-public class Main {
+public class MainFile {
 
     public static void main(String[] args) {
+
         SolarPanel solarPanel = new SolarPanel();
         OxygenGenerator oxygenGenerator = new OxygenGenerator();
 
+        System.out.println("=== Space Station Survival ===");
+
+        System.out.println("\nTesting equipment:");
         solarPanel.operate();
         oxygenGenerator.operate();
 
+        System.out.println("\nTesting repairs:");
         solarPanel.repair();
         oxygenGenerator.repair(20);
+
+        System.out.println("\nTesting conditions:");
+        System.out.println("Solar Panel: " + solarPanel.getCondition() + "%");
+        System.out.println("Oxygen Generator: " + oxygenGenerator.getCondition() + "%");
     }
 }
