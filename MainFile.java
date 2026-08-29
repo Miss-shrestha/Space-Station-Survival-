d7048492a281065684739404a0e0036e80389a8d
nimeshthapat11-dev <nimeshraj.thapa@students.mq.edu.au>
2026-08-29 13:20:02 +1000

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -1,5 +1,4 @@
 import javax.swing.JFrame;
-
 public class MainFile {
 
     public static void main(String[] args) {
@@ -7,14 +6,11 @@ public class MainFile {
         SolarPanel solarPanel = new SolarPanel();
         OxygenGenerator oxygenGenerator = new OxygenGenerator();
 
-        Station station = new Station();
-        station.addEquipment(solarPanel);
-        station.addEquipment(oxygenGenerator);
-
         System.out.println("=== Space Station Survival ===");
 
         System.out.println("\nTesting equipment:");
-        station.operateAll();
+        solarPanel.operate();
+        oxygenGenerator.operate();
 
         System.out.println("\nTesting repairs:");
         solarPanel.repair();
@@ -23,15 +19,11 @@ public class MainFile {
         System.out.println("\nTesting conditions:");
         System.out.println("Solar Panel: " + solarPanel.getCondition() + "%");
         System.out.println("Oxygen Generator: " + oxygenGenerator.getCondition() + "%");
-
-        Engineer engineer = new Engineer("Alex");
-        engineer.repairEquipment(solarPanel);
-
         JFrame window = new JFrame("Space Station Survival");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(700, 500);
-        window.add(new SimulationPanel(station));
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+window.setSize(700, 500);
+window.add(new SimulationPanel());
+window.setLocationRelativeTo(null);
+window.setVisible(true);
     }
 }
