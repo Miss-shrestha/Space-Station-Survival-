11aab3aaa7fff578710f2bfa14c43142d8d1069f
nimeshrajthapa-ui <nimeshraj.thapa@students.mq.edu.au>
2026-09-10 19:34:46 +1000

    Add generic astronaut crew list

--- AstronautTest.java
+++ AstronautTest.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+
 public class AstronautTest {
 
     public static void main(String[] args) {
+
+        // Create an Engineer
         Engineer alex = createEngineer("Alex");
+
         showAstronautInfo(alex);
         moveTowardsRocket(alex);
         boardRocket(alex);
         engineerRepairsEquipment(alex);
 
+        // Create a Photographer
         Photographer sam = createPhotographer("Sam");
+
         boardRocket(sam);
         photographerPreparesCamera(sam);
+
+        // Generic ArrayList that stores different types of Astronauts
+        ArrayList<Astronaut> crew = new ArrayList<>();
+
+        crew.add(alex);
+        crew.add(sam);
+
+        System.out.println("\n=== Crew tasks ===");
+
+        // Polymorphism - each astronaut performs their own task
+        for (Astronaut astronaut : crew) {
+            astronaut.performTask();
+        }
     }
 
     // Creates a new Engineer astronaut
@@ -24,7 +44,7 @@ public class AstronautTest {
         return new Photographer(name);
     }
 
-    // Shows an astronaut's name, health, and oxygen
+    // Shows an astronaut's information
     public static void showAstronautInfo(Astronaut astronaut) {
         System.out.println("\n=== Astronaut details ===");
         System.out.println("Name: " + astronaut.getName());
@@ -32,10 +52,12 @@ public class AstronautTest {
         System.out.println("Oxygen: " + astronaut.getOxygen());
     }
 
-    // Moves the astronaut towards the rocket, ready to board
+    // Moves an astronaut towards the rocket
     public static void moveTowardsRocket(Astronaut astronaut) {
         System.out.println("\n=== Moving towards the rocket ===");
-        System.out.println(astronaut.getName() + " is walking towards the rocket.");
+        System.out.println(
+            astronaut.getName() + " is walking towards the rocket."
+        );
     }
 
     // Astronaut boards the rocket
@@ -44,16 +66,23 @@ public class AstronautTest {
         astronaut.board();
     }
 
-    // Engineer repairs a piece of equipment
+    // Engineer repairs equipment
     public static void engineerRepairsEquipment(Engineer engineer) {
         System.out.println("\n=== Engineer repairing equipment ===");
+
         SolarPanel panel = new SolarPanel();
+
         engineer.repairEquipment(panel);
     }
 
-    // Photographer prepares the camera and takes a photo
-    public static void photographerPreparesCamera(Photographer photographer) {
-        System.out.println("\n=== Photographer preparing the camera ===");
+    // Photographer performs their special task
+    public static void photographerPreparesCamera(
+            Photographer photographer) {
+
+        System.out.println(
+            "\n=== Photographer preparing the camera ==="
+        );
+
         photographer.performTask();
     }
 }
